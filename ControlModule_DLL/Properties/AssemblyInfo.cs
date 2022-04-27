2cd9597447aef1a081216c3c2fb0172f0f3b5545
Jiri Konecny <jirka.kon96@gmail.com>
2022-04-27 15:40:01 +0200

    Adjust version

--- ControlModule_DLL/Properties/AssemblyInfo.cs
+++ ControlModule_DLL/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 // Můžete zadat všechny hodnoty nebo nastavit výchozí číslo buildu a revize
 // pomocí zástupného znaku * takto:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.04.263")]
-[assembly: AssemblyFileVersion("1.2.04.263")]
+[assembly: AssemblyVersion("1.2.04.271")]
+[assembly: AssemblyFileVersion("1.2.04.271")]
 [assembly: NeutralResourcesLanguage("en")]
